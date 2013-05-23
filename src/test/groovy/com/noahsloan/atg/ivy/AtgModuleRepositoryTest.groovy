7390a2f63479b43388e0965d62cfbe188e244472
Rajesh J Advani <radvani@sapient.com>
2013-05-23 20:17:06 +0530

    Added JUnit test case template.

---
+++ src/test/groovy/com/noahsloan/atg/ivy/AtgModuleRepositoryTest.groovy
@@ -0,0 +1,69 @@
+package com.noahsloan.atg.ivy;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+class AtgModuleRepositoryTest {
+
+	@Test
+	void testGetAtgRoot() {
+		//fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testGetAtgVersionedRoot() {
+		//fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testGetCustomRoot() {
+		//fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testGetDefaultJar() {
+		//fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testResolveAtgRoot() {
+		//fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testResolveVersionedAtgRoot() {
+		//fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testResolveCustomRoot() {
+		//fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testGet() {
+		//fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testGetResource() {
+		//fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testList() {
+		//fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testGetNewResolverObject() {
+		//fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testGetNewResolver() {
+		//fail("Not yet implemented"); // TODO
+	}
+
+}
